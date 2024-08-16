6140c266c13e3f8b095858c8040661ed843d3dc5
OliviaWindsor7 <oliviawindsor1024@gmail.com>
2024-08-15 23:08:06 -0700

    update order service for json dependency

--- order-service/src/main/java/com/chuwa/util/Feign/KafkaService.java
+++ order-service/src/main/java/com/chuwa/util/Feign/KafkaService.java
@@ -1,6 +1,9 @@
 package com.chuwa.util.Feign;
 
 import com.chuwa.entity.Order;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +12,10 @@ public class KafkaService {
     @Autowired
     private KafkaClient kafkaClient;
 
-    public void sendMessage(String key, Order order) {
-        kafkaClient.publish(key, order.toString());
+    public void sendMessage(String key, Order order) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        String jsonString = objectMapper.writeValueAsString(order);
+        kafkaClient.publish(key, jsonString);
     }
 }
